902492553021f5edb992d9bd773008ac749a30be
Sergey-hub02 <s.park190802@gmail.com>
2022-05-02 10:03:43 +0300

    Добавил в контроллер приложения методы, чтобы страницы могли отображаться на сервере Tomcat. Также исправил пути до ресурсов проекта (css-файлы, js-файлы, изображения и т.д.), чтобы они также отображались на сервере

--- src/main/java/ru/mirea/LangMasterController.java
+++ src/main/java/ru/mirea/LangMasterController.java
@@ -7,8 +7,59 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/langmaster")
 public class LangMasterController {
+  /**
+   * Отображает главную страницу при GET-запросе
+   * на адрес /langmaster
+   */
   @GetMapping()
-  public String index() {
+  public String displayIndexPage() {
     return "index";
+  }
+
+  /**
+   * Отображает страницу профиля пользователя при
+   * GET-запросе на адрес /langmaster/profile
+   */
+  @GetMapping("/profile")
+  public String displayProfilePage() {
+    return "pages/profile";
+  }
+
+  /**
+   * Отображает страницу регистрации при
+   * GET-запросе на адрес /langmaster/register
+   */
+  @GetMapping("/register")
+  public String displayRegisterPage() {
+    return "pages/register";
+  }
+
+  /**
+   * Отображает страницу авторизации при
+   * GET-запросе на адрес /langmaster/login
+   */
+  @GetMapping("/login")
+  public String displayLoginPage() {
+    return "pages/login";
+  }
+
+  /**
+   * Отображает страницу курса при
+   * GET-запросе на адрес /langmaster/course
+   */
+  // TODO: добавить @PathVariable для id курса
+  @GetMapping("/course")
+  public String displayCoursePage() {
+    return "pages/course";
+  }
+
+  /**
+   * Отображает страницу уроков курса при
+   * GET-запросе на адрес /langmaster/course/lesson
+   */
+  // TODO: добавить @PathVariable для id урока
+  @GetMapping("/course/lesson")
+  public String displayLessonsPage() {
+    return "pages/lessons";
   }
 }
